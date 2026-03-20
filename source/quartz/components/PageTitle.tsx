a1f026d75253b910e6fe0a3c326f36209ae3db35
marecini <marecini@proton.me>
2026-03-20 15:47:51 +0100

    changes to site

--- source/quartz/components/PageTitle.tsx
+++ source/quartz/components/PageTitle.tsx
@@ -1,22 +1,32 @@
 import { pathToRoot } from "../util/path"
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
 import { classNames } from "../util/lang"
-import { i18n } from "../i18n"
 
 const PageTitle: QuartzComponent = ({ fileData, cfg, displayClass }: QuartzComponentProps) => {
-  const title = cfg?.pageTitle ?? i18n(cfg.locale).propertyDefaults.title
   const baseDir = pathToRoot(fileData.slug!)
   return (
     <h2 class={classNames(displayClass, "page-title")}>
-      <a href={baseDir}>{title}</a>
+      <a href={baseDir}>~ / writeups</a>
     </h2>
   )
 }
 
 PageTitle.css = `
 .page-title {
-  font-size: 1.75rem;
+  font-size: 1.2rem;
   margin: 0;
+  font-family: 'Courier New', Courier, monospace;
+  color: var(--purple);
+  letter-spacing: 0.05em;
+}
+
+.page-title a {
+  color: var(--purple) !important;
+}
+
+.page-title a:hover {
+  text-shadow: 0 0 8px var(--purple);
+  opacity: 0.85;
 }
 `
 
